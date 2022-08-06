35ad03f6bf9eec9d0c412df090a85b7e0fd704bb
BlueRock96 <akaash.chinna@gmail.com>
2022-08-06 12:13:04 +0530

    finalport

--- src/components/authServices/withAuth.js
+++ src/components/authServices/withAuth.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import AuthService from './AuthService';
 
 export default function withAuth(AuthComponent) {
-    const Auth = new AuthService('http://localhost:7001');
+    const Auth = new AuthService('http://localhost:82');
     return class AuthWrapped extends Component {
         constructor() {
             super();
